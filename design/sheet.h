e7af11032e727b367edb8ae953e1585849a86432
EvgeniiMokeichev <117139151+DerMakar@users.noreply.github.com>
2023-07-20 17:08:58 +0300

    Case done

--- design/sheet.h
+++ design/sheet.h
@@ -1,18 +1,27 @@
 #pragma once
 
-#include "cell.h"
+
 #include "common.h"
 
 #include <functional>
+#include <deque>
+
+class Cell;
 
 class Sheet : public SheetInterface {
 public:
-    ~Sheet();
+    Sheet() = default;
+
+    ~Sheet() = default;
 
     void SetCell(Position pos, std::string text) override;
 
+    void SetEmptyCell(Position pos);
+
     const CellInterface* GetCell(Position pos) const override;
     CellInterface* GetCell(Position pos) override;
+
+    //const std::vector<std::vector<std::unique_ptr<Cell>>>& GetSheet() const;
 
     void ClearCell(Position pos) override;
 
@@ -21,14 +30,11 @@ public:
     void PrintValues(std::ostream& output) const override;
     void PrintTexts(std::ostream& output) const override;
 
-    const Cell* GetConcreteCell(Position pos) const;
-    Cell* GetConcreteCell(Position pos);
+    bool IsValid(Position pos) const;
 
+   
 private:
-    void MaybeIncreaseSizeToIncludePosition(Position pos);
-    void PrintCells(std::ostream& output,
-                    const std::function<void(const CellInterface&)>& printCell) const;
-    Size GetActualSize() const;
-
-    std::vector<std::vector<std::unique_ptr<Cell>>> cells_;
+    mutable std::vector<std::vector<std::unique_ptr<Cell>>> sheet_;
+    Size sheet_size_;
 };
+
